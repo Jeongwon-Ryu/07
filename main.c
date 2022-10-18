b32f22bec9192ef682597979831edf052bf080e4
Jeongwon-Ryu <113072586+Jeongwon-Ryu@users.noreply.github.com>
2022-10-18 16:41:24 +0900

    03

--- main.c
+++ main.c
@@ -5,15 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-
-	for (i=0; i<5;i++)
-	{
-		int temp =1 ;
-		printf("temp = %d\n",temp);
-		temp++;
-	}
-	
+	int i=10;
+	printf("함수 호출 전i=%d\n",i);
+	inc(i);
+	printf("함수 호출 후i=%d\n",i);
 	return 0;
+	
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
